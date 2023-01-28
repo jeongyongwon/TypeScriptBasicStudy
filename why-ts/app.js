4395014857b08ebdddbf45a6371657cf567794f3
정용원 <yongdev>
2023-01-28 21:59:26 +0900

    compile from ts to js

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,6 +9,33 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ *  @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+
+/**
+ *  @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * 
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url)
+}
+
+fetchUser().then(function(response)) {
+  response.address.city;
+}
+
+
 function startApp() {
   axios
     .get(url)
